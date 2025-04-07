9c2a74c70ade1ef847c2d1a3cfbf0902dfd642ca
DenisKotsyuba <kocubads96@gmail.com>
2025-04-07 13:53:29 +0300

    refactor(turnstile): simplify ITurnstileController interface
    Removed redundant methods and updated `getToken` to return a Promise.
    This streamlines the interface for improved usability and clarity.

--- libs/models/src/turnstile/turnstile.controller.ts
+++ libs/models/src/turnstile/turnstile.controller.ts
@@ -1,8 +1,5 @@
 import { InitializingEntity } from '../base'
 
 export interface ITurnstileController extends InitializingEntity {
-  getToken(): string | null
-  turnstileComplete(): Promise<void>
-  getVerificationInProgress(): boolean
-  startTurnstile(): void
+  getToken(): Promise<string>
 }
