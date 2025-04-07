903286de2e7b1ad22b43bed97af509c195732df2
DenisKotsyuba <kocubads96@gmail.com>
2025-04-07 13:54:18 +0300

    feat(api): add token list for One Inch dev portal
    Added a comprehensive token list JSON file to the One Inch development portal under `libs/sdk/src/one-inch-dev-portal/api/onchain/token-list.json`. This includes details about various tokens, their chain IDs, addresses, decimals, logos, and tags. This will enhance on-chain token data accessibility.

--- libs/sdk/src/one-inch-dev-portal/api/onchain/one-inch-dev-portal-cross-chain-onchain.adapter.ts
+++ libs/sdk/src/one-inch-dev-portal/api/onchain/one-inch-dev-portal-cross-chain-onchain.adapter.ts
@@ -86,16 +86,16 @@ export class OneInchDevPortalCrossChainOnChainAdapter
     return mergeProxyResultBalance(result.flat())
   }
 
+  async getTokenList(): Promise<ITokenV2Dto[]> {
+    return await import('./token-list.json').then((data) => data.default as ITokenV2Dto[])
+  }
+
   getGasPrice(): Promise<GasPriceDto | null> {
     throw new Error('Method not implemented.')
   }
 
   getTokenPrice(): Promise<ProxyResultTokenPrice> {
     throw new Error('Method not implemented.')
-  }
-
-  getTokenList(): Promise<ITokenV2Dto[]> {
-    throw new Error('OneInchDevPortalCrossChainOnChainAdapter not supported getTokenList call')
   }
 
   getProxyClient(): IProxyClient {
