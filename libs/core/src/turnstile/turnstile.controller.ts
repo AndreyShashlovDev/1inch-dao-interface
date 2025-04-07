b92d1c7e5c261c2ab833faeb53ea46d000210b49
DenisKotsyuba <kocubads96@gmail.com>
2025-04-07 13:52:51 +0300

    refactor(turnstile): simplify and optimize TurnstileController
    Refactored TurnstileController to streamline token retrieval and error handling. Introduced `@CacheActivePromise` for caching, removed deprecated methods, and enhanced error retry logic. Improved code clarity and reduced duplication.

--- libs/core/src/turnstile/turnstile.controller.ts
+++ libs/core/src/turnstile/turnstile.controller.ts
@@ -1,5 +1,5 @@
 import { IApplicationContext, ITurnstileController } from '@1inch-community/models'
-import { firstValueFrom, Subject } from 'rxjs'
+import { CacheActivePromise } from '../decorators'
 import { appendStyle } from '../lit-utils'
 import { lazyAppContext } from '../utils'
 
@@ -33,123 +33,104 @@ declare global {
   }
 }
 
-const TurnstileRetry: string[] = [
-  'crashed',
-  'undefined_error',
-  'challenge_failed',
-  '102',
-  '103',
-  '104',
-  '110600',
-  '300100',
-  '600',
-]
+const CONTAINER_ID = 'turnstile-container'
 
 const MAX_RETRY_COUNT = 4
 
 export class TurnstileController implements ITurnstileController {
-  private readonly complete$ = new Subject<void>()
   private readonly context = lazyAppContext('TurnstileController')
-  private element: HTMLDivElement | null = null
-  private siteKey: string | null = null
-  private token: string | null = null
-  private retryCounter = 0
-  private verificationInProgress = false
-  private isInitScript = false
+  private script: HTMLScriptElement | null = null
 
   async init(context: IApplicationContext): Promise<void> {
     this.context.set(context)
-    this.siteKey = this.context.value.environment.get('cloudflareTurnstileSiteKey') ?? null
-    if (!this.siteKey) throw new Error('TurnstileController.init Error: siteKey is not defined')
-    this.initScript()
   }
 
-  getToken(): string | null {
-    const token = this.token
-    this.token = null
-    return token
+  @CacheActivePromise()
+  async getToken(): Promise<string> {
+    await this.initScript()
+    const container = this.renderContainer()
+    return await this.renderToken(container)
   }
 
-  getVerificationInProgress(): boolean {
-    return this.verificationInProgress
-  }
+  private async initScript(): Promise<void> {
+    if (this.script) return
+    return await new Promise((resolve) => {
+      const script = document.createElement('script')
+      script.type = 'text/javascript'
+      script.async = true
+      script.defer = true
+      script.src = `https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit&onload=${CALLBACK_NAME}`
 
-  turnstileComplete(): Promise<void> {
-    return firstValueFrom(this.complete$)
-  }
-
-  startTurnstile() {
-    if (this.verificationInProgress || !this.isInitScript) return
-    this.clean()
-    this.render()
-  }
-
-  private initScript() {
-    if (this.isInitScript) return
-    const script = document.createElement('script')
-    script.type = 'text/javascript'
-    script.async = true
-    script.defer = true
-    script.src = `https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit&onload=${CALLBACK_NAME}`
-
-    Reflect.set(window, CALLBACK_NAME, () => {
-      this.isInitScript = true
-      this.render()
+      Reflect.set(window, CALLBACK_NAME, resolve)
+      document.head.appendChild(script)
     })
-
-    document.head.appendChild(script)
   }
 
-  private render() {
-    if (!this.siteKey || !window.turnstile) return
-    this.verificationInProgress = true
-    this.element = document.createElement('div')
-    appendStyle(this.element, {
+  private renderContainer(): HTMLDivElement {
+    const container = document.createElement('div')
+    appendStyle(container, {
       position: 'absolute',
       zIndex: '-1',
       top: '-1000px',
       left: '-1000px',
     })
-    document.body.appendChild(this.element)
-    window.turnstile.render(this.element, {
-      sitekey: this.siteKey,
-      callback: (token) => this.setToken(token),
-      'error-callback': (err) => this.handleError(`${err}`),
+    container.id = CONTAINER_ID
+    document.body.appendChild(container)
+    return container
+  }
+
+  private async renderToken(container: HTMLDivElement): Promise<string> {
+    const siteKey = this.context.value.environment.get('cloudflareTurnstileSiteKey') ?? null
+    if (!siteKey) throw new Error('TurnstileController.renderToken Error: siteKey is not defined')
+    return await new Promise((resolve, reject) => {
+      if (!window.turnstile)
+        throw new Error('TurnstileController.renderToken Error: window.turnstile is defined')
+      let retryCount = 0
+
+      const errorHandler = (err: string) => {
+        if (retryCount >= MAX_RETRY_COUNT || !isSupportRetryError(err)) {
+          reject(err)
+          return
+        }
+        window.turnstile!.reset(CONTAINER_ID)
+        retryCount++
+      }
+
+      const callback = (token: string) => {
+        try {
+          window.turnstile!.remove(CONTAINER_ID)
+        } catch (error) {
+          console.warn(error)
+        }
+        resolve(token)
+      }
+
+      window.turnstile.render(container, {
+        sitekey: siteKey,
+        callback,
+        'error-callback': errorHandler,
+      })
     })
   }
 
-  private setToken(token: string) {
-    this.token = token
-    this.clean()
-    this.retryCounter = 0
-    this.verificationInProgress = false
-    this.complete$.next()
-  }
+  // private clean() {
+  //   if (!this.element || !this.element.parentElement) return
+  //   window.turnstile?.remove(this.element)
+  //   document.body.removeChild(this.element)
+  //   this.element = null
+  // }
+}
 
-  private clean() {
-    if (!this.element || !this.element.parentElement) return
-    window.turnstile?.remove(this.element)
-    document.body.removeChild(this.element)
-    this.element = null
-  }
+const TurnstileRetry: string[] = ['crashed', 'undefined_error', 'challenge_failed']
 
-  private handleError(err: string) {
-    console.warn('Turnstile render error', err)
-    if (
-      this.retryCounter < MAX_RETRY_COUNT &&
-      TurnstileRetry.find((code) => err.startsWith(code))
-    ) {
-      this.retry()
-    } else {
-      this.clean()
-    }
-    this.verificationInProgress = false
-  }
+// https://developers.cloudflare.com/turnstile/troubleshooting/client-side-errors/error-codes/#error-code-300-and-600
+const TurnstileRetryCode: number[] = [102, 103, 104, 106, 300, 600]
 
-  private retry() {
-    console.warn('Turnstile retry')
-    this.retryCounter++
-    this.clean()
-    this.render()
+function isSupportRetryError(error: string): boolean {
+  if (TurnstileRetry.includes(error)) {
+    return true
   }
+  const errorCore = parseInt(error.slice(0, 3), 10)
+  if (isNaN(errorCore)) return false
+  return TurnstileRetryCode.includes(errorCore)
 }
