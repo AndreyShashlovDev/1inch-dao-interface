8e2dc7f360bf94ca7e14025e2a8ee778caff4f8e
DenisKotsyuba <kocubads96@gmail.com>
2025-04-11 17:18:20 +0300

    feat(integration-layer): add overlayFactory for overlay handling
    Added `overlayFactory` to both embedded and application contexts to support dynamic loading of the OverlayController from `@1inch-community/ui-components`. This enhances UI overlay management.

--- libs/integration-layer/src/embedded/context.ts
+++ libs/integration-layer/src/embedded/context.ts
@@ -45,6 +45,10 @@ export async function bootstrapApplicationContext(config: EmbeddedBootstrapConfi
         import('@1inch-community/core/animations').then((m) => new m.AnimationsManager()),
       environmentFactory: () =>
         import('@1inch-community/core/environment').then((m) => new m.EnvironmentController(env)),
+      overlayFactory: () =>
+        import('@1inch-community/ui-components/overlay').then(
+          (m) => new m.OverlayController('#app-root')
+        ),
       swapContextFactory: (context) =>
         import('@1inch-community/sdk/swap').then((m) => {
           const swapContext = new m.SwapContext(context)
