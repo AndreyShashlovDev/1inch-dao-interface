5158c0229d56b441c18a392a0c836971c22954dd
DenisKotsyuba <kocubads96@gmail.com>
2025-04-11 17:07:57 +0300

    refactor(chain-selector): simplify components and optimize overlays
    Refactored chain-selector components for better readability and modularity:
    - Replaced custom scroll-container logic with Lit Map directive for consistency.
    - Updated `inch-card-header` structure and styling.
    - Simplified overlay handling by leveraging ApplicationContext.
    - Removed redundant and commented-out code for improved maintainability.
    
    BREAKING CHANGE: Modifications in methods and component structure may require updates in dependent implementations.

--- libs/widgets/src/chain-selector/elements/chain-selector-list/chain-selector-list.element.ts
+++ libs/widgets/src/chain-selector/elements/chain-selector-list/chain-selector-list.element.ts
@@ -6,6 +6,7 @@ import '@1inch-community/ui-components/card'
 import '@1inch-community/ui-components/scroll'
 import { html, LitElement } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
+import { map as LitMap } from 'lit/directives/map.js'
 import '../chain-selector-list-item'
 import { chainSelectorListStyle } from './chain-selector-list.style'
 
@@ -22,28 +23,49 @@ export class ChainSelectorListElement extends LitElement {
       this.selectedChainViewList.length === chainList.length ? 'Deselect All' : 'Select All'
     return html`
       <inch-card overlayView>
-        <header class="header">
-          <h2 class="title">Networks</h2>
-          <inch-button size="xl" type="link" @click="${this.onSelectAllClick}">
+        <inch-card-header separator mini>
+          <span slot="left-container" class="title">Networks</span>
+          <inch-button
+            slot="right-container"
+            size="xl"
+            type="link"
+            @click="${this.onSelectAllClick}"
+          >
             <inch-text-animate text="${text}"></inch-text-animate>
           </inch-button>
-        </header>
+        </inch-card-header>
         ${this.getList()}
       </inch-card>
     `
   }
 
   private getList() {
-    return chainList.map(
-      (info) => html`
-        <inch-chain-selector-list-item
-          .info="${info}"
-          .isActiveChain="${this.selectedChainViewList.includes(info)}"
-          @chainItemClick="${(event: CustomEvent) =>
-            this.onChainItemClick(event.detail.value as ChainViewFull)}"
-        ></inch-chain-selector-list-item>
-      `
-    )
+    return html`
+      <inch-scroll-view-consumer>
+        ${LitMap(
+          chainList,
+          (info) => html`
+            <inch-chain-selector-list-item
+              .info="${info}"
+              .isActiveChain="${this.selectedChainViewList.includes(info)}"
+              @chainItemClick="${(event: CustomEvent) =>
+                this.onChainItemClick(event.detail.value as ChainViewFull)}"
+            ></inch-chain-selector-list-item>
+          `
+        )}
+      </inch-scroll-view-consumer>
+    `
+
+    // return chainList.map(
+    //   (info) => html`
+    //     <inch-chain-selector-list-item
+    //       .info="${info}"
+    //       .isActiveChain="${this.selectedChainViewList.includes(info)}"
+    //       @chainItemClick="${(event: CustomEvent) =>
+    //         this.onChainItemClick(event.detail.value as ChainViewFull)}"
+    //     ></inch-chain-selector-list-item>
+    //   `
+    // )
   }
 
   private resetSelectedChainViewList() {
--- libs/widgets/src/chain-selector/elements/chain-selector-list/chain-selector-list.style.ts
+++ libs/widgets/src/chain-selector/elements/chain-selector-list/chain-selector-list.style.ts
@@ -7,47 +7,11 @@ export const chainSelectorListStyle = css`
     min-width: 320px;
   }
 
-  .scroll-container {
-    position: relative;
-    height: 100%;
-    overflow-y: auto;
-  }
-
-  .scroll-overlay {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-  }
-
-  .header {
-    position: relative;
-
-    display: flex;
-    padding: 8px 8px 16px;
-    justify-content: space-between;
-  }
-
-  .header::after {
-    position: absolute;
-    left: -8px;
-    bottom: 0;
-
-    display: block;
-    width: calc(100% + 16px);
-    height: 1px;
-
-    background-color: var(--color-border-border-tertiary);
-
-    content: '';
-  }
-
   .title {
-    margin: 0;
-
-    color: var(--color-content-content-primary);
-    font-size: 16px;
     font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    letter-spacing: 0;
+    color: var(--color-content-content-primary);
   }
 `
