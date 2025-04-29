4ae6383bccd17bfa7b64ba586a7a6352542535be
DenisKotsyuba <kocubads96@gmail.com>
2025-04-29 19:09:59 +0300

    feat(theme): enhance scrollbar styling in scrollbar.style.ts
    Added custom styles for webkit scrollbars, including width, track, and handle. Styles also account for hover state and scrollbar visibility based on `hideScrollBar` property.

--- libs/core/src/theme/styles/scrollbar.style.ts
+++ libs/core/src/theme/styles/scrollbar.style.ts
@@ -9,6 +9,38 @@ export const getScrollbarStyle = (hostName: string, hideScrollBar?: boolean) => 
     scrollbar-width: ${unsafeCSS(hideScrollBar ? 'none' : 'thin')};
     scrollbar-gutter: stable;
   }
+
+  /* width */
+  ${unsafeCSS(hostName)}::-webkit-scrollbar {
+    width: 4px;
+    height: 4px;
+  }
+
+  /* Track */
+  ${unsafeCSS(hostName)}::-webkit-scrollbar-track {
+    background: transparent;
+  }
+
+  /* Handle */
+  ${unsafeCSS(hostName)}::-webkit-scrollbar-thumb {
+    background: var(--primary);
+    border-radius: 4px;
+  }
+
+  /* Handle on hover */
+  ${unsafeCSS(hostName)}::-webkit-scrollbar-thumb:hover {
+    background: var(--primary-hover);
+  }
+
+  ${unsafeCSS(
+    hideScrollBar
+      ? css`
+          ${unsafeCSS(hostName)}::-webkit-scrollbar {
+            display: none;
+          }
+        `
+      : ''
+  )}
 `
 
 export const scrollbarStyle = getScrollbarStyle(':host')
