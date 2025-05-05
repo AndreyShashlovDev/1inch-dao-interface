3f15e2a2468422bd2a2e8f39444672eb0671f382
DenisKotsyuba <kocubads96@gmail.com>
2025-05-05 20:16:56 +0300

    refactor(rate): update rate types to use IBigFloat
    Replaced `bigint` with `IBigFloat` for `rate` and `revertedRate` in the `Rate` type.
    This enhances precision and consistency for handling token rate calculations.

--- libs/models/src/token-price/rate.ts
+++ libs/models/src/token-price/rate.ts
@@ -1,11 +1,12 @@
+import type { IBigFloat } from '../big-float'
 import type { ChainId } from '../chain'
 import type { IToken } from '../token'
 
 export type Rate = {
   chainId: ChainId
   isReverted: boolean
-  rate: bigint
-  revertedRate: bigint
+  rate: IBigFloat
+  revertedRate: IBigFloat
   sourceToken: IToken
   destinationToken: IToken
 }
